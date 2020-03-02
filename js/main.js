caa4563d9e7b53faa28a1cf0ee4cac7d91854f0e
sovaz1997 <sovaz1997@yandex.ru>
2020-03-02 16:28:04 +0300

    feat: add inheritance for control and regular keys

--- js/main.js
+++ js/main.js
@@ -8,12 +8,9 @@ class KeyState {
     this.ruOnShift = ruOnShift;
   }
 }
-
 class Key {
   constructor(keyState) {
-    this.keyState = keyState;
     this.button = document.createElement('button');
-    this.button.textContent = this.keyState.def;
     this.button.classList.add('key');
   }
   
@@ -36,11 +33,28 @@ class Key {
   }
 }
 
+class LetterKey extends Key {
+  constructor(keyState) {
+    super();
+    this.keyState = keyState;
+    this.button.textContent = this.keyState.def;
+  }
+}
+
+class ControlKey extends Key {
+
+}
+
 class Keyboard {
   constructor(selectorStr) {
     this.keys = [];
     this.keyMap = [];
     this.selector = document.querySelector(selectorStr);
+
+    this.state = {
+      english: true,
+      shift: false
+    };
 
     document.addEventListener("keydown", (e) => {
       const keyObject = this.keyMap[e.key];
@@ -57,8 +71,8 @@ class Keyboard {
     });
   }
 
-  addKey(keyState) {
-    const key = new Key(keyState);
+  addLetterKey(keyState) {
+    const key = new LetterKey(keyState);
 
     this.keyMap[keyState.def] = key;
     this.keyMap[keyState.onShift] = key;
@@ -74,4 +88,4 @@ class Keyboard {
 }
 
 const keyboard = new Keyboard('body');
-keyboard.addKey(new KeyState('a', 'A', 'ф', 'Ф'));
+keyboard.addLetterKey(new KeyState('a', 'A', 'ф', 'Ф'));
